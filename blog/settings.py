a681f5b248a9e79255f5f06fe13b08e7cfd97bf2
mrjas <mrjasonnlin@gmail.com>
2022-08-01 10:01:56 +0800

    hh

--- blog/settings.py
+++ blog/settings.py
@@ -14,8 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
 
-
+# For Heroku deployment
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -134,7 +139,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# For Heroku deployment
-STATIC_ROOT = 'staticfiles'
