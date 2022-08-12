225b7feab13ff25009d7b2588c0174e3ed09409f
mrjas <mrjasonnlin@gmail.com>
2022-08-12 10:10:44 +0800

    Default Changelist

--- blog/settings.py
+++ blog/settings.py
@@ -16,7 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # For Heroku deployment
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
