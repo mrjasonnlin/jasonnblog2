c146de556a283a0d73fcc2cd7f45a504ed4339c0
mrjas <mrjasonnlin@gmail.com>
2022-09-23 12:26:32 +0800

    Default Changelist

--- blog/urls.py
+++ blog/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+# from django.conf.urls import url
+# from django.urls import path,include
 from django.contrib import admin
 
 urlpatterns = [
